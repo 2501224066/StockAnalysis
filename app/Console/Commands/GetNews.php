fb37536c8ff0d36d3ec0629fb00589b7cf481da8
figurant <2501224066@qq.com>
2019-09-27 13:34:02 +0800

    get news after 160 s

--- app/Console/Commands/GetNews.php
+++ app/Console/Commands/GetNews.php
@@ -22,7 +22,7 @@ class GetNews extends Command
     public function handle()
     {
         // 目标地址  时间减少60秒，获取60秒区间内容
-        $url = "https://news.10jqka.com.cn/tapp/news/push/stock/?page=1&tag=&track=website&ctime=". (time()-60);
+        $url = "https://news.10jqka.com.cn/tapp/news/push/stock/?page=1&tag=&track=website&ctime=". (time()-180);
         //伪造浏览器UA
         $headers = [
             'user-agent' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
